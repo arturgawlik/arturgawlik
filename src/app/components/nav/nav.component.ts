7ebd6f54ec89700ec06bf030774b4f85e6b5bebe
arturgawlik <artur.gawlik@icloud.com>
2020-04-05 01:24:05 +0200

    changes for scully

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -8,18 +8,10 @@ import { Router } from '@angular/router';
 })
 export class NavComponent implements OnInit {
 
-  constructor(private router: Router) {
+  constructor(public router: Router) {
    }
 
   ngOnInit() {
-  }
-
-  hfClick() {
-    this.router.navigate(['']);
-  }
-
-  cvClick() {
-    this.router.navigate(['cv']);
   }
 
 }
